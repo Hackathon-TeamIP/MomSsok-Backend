71ff7a9a7d00c66d4b8e0c4bbd41d716a7b44aaa
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-08-22 15:39:07 +0900

    first commit

---
+++ MomSsok/src/main/java/MomSsok/MomSsok/MomSsokApplication.java
@@ -0,0 +1,13 @@
+package MomSsok.MomSsok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomSsokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomSsokApplication.class, args);
+	}
+
+}
---
+++ MomSsok/src/test/java/MomSsok/MomSsok/MomSsokApplicationTests.java
@@ -0,0 +1,13 @@
+package MomSsok.MomSsok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomSsokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
