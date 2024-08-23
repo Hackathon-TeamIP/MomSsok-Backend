48ac48a7a59ac450e8d71fd6c211e7423b554429
acdc0611@naver.com <ansdj@8599>
2024-08-23 20:09:16 +0900

    DB 연동 테스트 코드

---
+++ MomSsok/src/main/java/MomSsok/MomSsok/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package MomSsok.MomSsok.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserDTO {
+
+    private Integer id;
+    private String username;
+    private String email;
+
+
+    // 필요시 비밀번호를 포함할 수 있지만, 보안을 위해 일반적으로 DTO에서는 제외합니다.
+    // private String password;
+
+    // 생성자, getter, setter 등을 필요에 따라 추가할 수 있습니다.
+}
